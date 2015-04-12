a51a43d581041bc1bf22c75614bf5f465cd00b6f
Sn0wCh1ld <nojailbreakapps@outlook.com>
2015-04-12 16:25:54 -0400

    changes

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #jaybeeceeTEST3\r");
+            Send(hSocket, "WHO #jaybeeceeTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #jaybeecee00-#jaybeecee99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #jaybeecee%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #jaybeecee%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
